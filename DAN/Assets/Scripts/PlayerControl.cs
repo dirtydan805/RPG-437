9bd3753932518418930a191b15cf004f80b33d25
dirtydan805 <35823973+dirtydan805@users.noreply.github.com>
2018-02-01 19:11:07 -0800

    Add files via upload

---
+++ DAN/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+	
+private RigidBody rb;
+
+	void Start()
+	{
+		rb = GetComponent <RigidBody>();
+	}
+	
+	void Update()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+	
+		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+		
+		rb.AddForce (movement);
+	}
+}
+
