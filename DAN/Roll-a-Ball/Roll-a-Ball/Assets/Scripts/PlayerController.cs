58131691f31aa37bf62a76b8f694305ce82a151b
dirtydan805 <35823973+dirtydan805@users.noreply.github.com>
2018-02-09 17:48:09 -0800

    update
    changed ball
    changed plane
    added wall
    changed wall
    added jump
    changed speed

--- DAN/Roll-a-Ball/Roll-a-Ball/Assets/Scripts/PlayerController.cs
+++ DAN/Roll-a-Ball/Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,28 @@ public class PlayerController : MonoBehaviour
 {
 
 	public float speed = 800.0f;
+	public float jumpSpeed = 100.0f;
+	private bool onGround = false;
+	Rigidbody rb;
+
 
 	void FixedUpdate()
 	{
+		
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-
+		rb=GetComponent<Rigidbody>();
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
-	}
 
+		if (Input.GetKeyDown("space"))
+		{
+			rb.AddForce(Vector3.up * jumpSpeed);
+		}
+	}
+	void OnCollisionStay ()
+	{
+		onGround = true;
+	}
 }
