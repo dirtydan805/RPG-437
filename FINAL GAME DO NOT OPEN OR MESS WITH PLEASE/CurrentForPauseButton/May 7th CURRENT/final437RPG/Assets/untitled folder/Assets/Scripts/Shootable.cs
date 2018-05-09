5f8207362842c64f23baa97ec06405b074974519
Gabriel Espinosa <gabrielespinosa@Gabriels-MacBook-Pro.local>
2018-05-08 19:11:43 -0700

    YO
    YOOO

---
+++ FINAL GAME DO NOT OPEN OR MESS WITH PLEASE/CurrentForPauseButton/May 7th CURRENT/final437RPG/Assets/untitled folder/Assets/Scripts/Shootable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shootable : MonoBehaviour {
+	public int currentHealth = 100;
+
+	public void Damage(int damageAmount)
+	{
+		currentHealth -= damageAmount;
+		if(currentHealth <=0)
+		{
+			gameObject.SetActive (false);
+	}
+
+}
+}
