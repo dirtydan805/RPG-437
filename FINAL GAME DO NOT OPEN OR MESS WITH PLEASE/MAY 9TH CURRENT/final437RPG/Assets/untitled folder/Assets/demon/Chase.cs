4c34a84af0e0283ff8e925a4567bed69f345d29f
dirtydan805 <35823973+dirtydan805@users.noreply.github.com>
2018-05-09 14:05:56 -0700

    MAY 9TH
    BUILD

---
+++ FINAL GAME DO NOT OPEN OR MESS WITH PLEASE/MAY 9TH CURRENT/final437RPG/Assets/untitled folder/Assets/demon/Chase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour {
+	public Transform target;
+	public Transform myTransform;
+
+	void Update()
+	{
+		transform.LookAt (target);
+		transform.Translate (Vector3.forward * 5 * Time.deltaTime);
+	}
+
+}
