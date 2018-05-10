a4967d50fee3264915f16dd1485d91dbd1b6f624
Gabriel Espinosa <gabrielespinosa@Gabriels-MacBook-Pro.local>
2018-05-09 17:09:57 -0700

    YOOO
    YOOOOO

---
+++ FINAL GAME DO NOT OPEN OR MESS WITH PLEASE/MostCurrent May 9/MAY 9TH CURRENT/final437RPG/Assets/untitled folder/Assets/Scripts/sound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound : MonoBehaviour {
+
+	public AudioClip currentMessage;
+	public float Volume;
+	AudioSource audio;
+	public bool alreadyPlayed = false;
+
+	void Start(){
+		audio = GetComponent<AudioSource> ();
+	}
+
+	void OnTriggerEnter(){
+		if (!alreadyPlayed) {
+			audio.PlayOneShot (currentMessage, Volume);
+			alreadyPlayed = true;
+		}
+	}
+}
