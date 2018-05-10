6ffa1eee40820ca481107ef118b0bd03f7e56f39
Gabriel Espinosa <gabrielespinosa@Gabriels-MacBook-Pro.local>
2018-05-09 22:34:23 -0700

    YOOOO
    YOOOOOOOO

---
+++ FINAL GAME DO NOT OPEN OR MESS WITH PLEASE/This is the most current/MostCurrent May 9/MAY 9TH CURRENT/final437RPG/Assets/EnemyFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+
+
+	public float speed;
+	public float stoppingDistance;
+
+	private Transform target;
+
+
+	// Use this for initialization
+	void Start () {
+
+		target = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+	}
+	void Update()
+	{
+		if (Vector3.Distance (transform.position, target.position) > 3) {
+			transform.position = Vector3.MoveTowards (transform.position, target.position, speed * Time.deltaTime);
+		}
+	}
+	
+
+}
