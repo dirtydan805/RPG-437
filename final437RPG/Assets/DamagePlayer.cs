63462f846d481de6e31828937bfea50a2d8234b0
Gabriel Espinosa <gabrielespinosa@Gabriels-MacBook-Pro.local>
2018-05-10 19:55:53 -0700

    Hopefully fix this

---
+++ final437RPG/Assets/DamagePlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour {
+
+	public int playerHealth = 30;
+	int damage = 10;
+
+	// Use this for initialization
+	void Start () {
+		print (playerHealth);
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "enemyDong") {
+			playerHealth -= damage;
+			print (" Just hit" + playerHealth);
+		}
+	}
+
+}
