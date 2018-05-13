eb801ee400d02a07c06668e22e78bcd1d4ab64e9
Gabriel Espinosa <gabrielespinosa@Gabriels-MacBook-Pro.local>
2018-05-13 02:06:20 -0700

    Almost

--- version 2/Assets/EnemyFollow.cs
+++ version 2/Assets/EnemyFollow.cs
@@ -9,12 +9,15 @@ public class EnemyFollow : MonoBehaviour {
 	//public float stoppingDistance;
 
 	public Transform player;
+	static Animator anim;
 
 
 	// Use this for initialization
 	void Start () {
 
 		//target = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+		anim = GetComponent<Animator>();
+
 	}
 	void Update()
 	{
@@ -22,15 +25,23 @@ public class EnemyFollow : MonoBehaviour {
 		//	transform.position = Vector3.MoveTowards (transform.position, target.position, speed * Time.deltaTime);
 		//}
 
-		if (Vector3.Distance (player.position, this.transform.position) < 100) 
-		{
+		if (Vector3.Distance (player.position, this.transform.position) < 10000) {
 			Vector3 direction = player.position - this.transform.position;
 			direction.y = 0;
+			anim.SetBool ("isIdle", false);
 			this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (direction), 0.1f);
-			if (direction.magnitude > 5) 
-			{
+			if (direction.magnitude > 5) {
 				this.transform.Translate (0, 0, 0.05f);
+				anim.SetBool ("isWalking", true);
+				anim.SetBool ("isAttacking", false);
+			} else {
+				anim.SetBool ("isAttacking", true);
+				anim.SetBool ("isWalking", false);
 			}
+		} else {
+			anim.SetBool ("isIdle", true);
+			anim.SetBool ("isWalking", false);
+			anim.SetBool ("isAttacking", false);
 		}
 	}
 	
