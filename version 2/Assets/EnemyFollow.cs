f81c5308b4e5f558b774062ba95635b3df04ace3
Gabriel Espinosa <gabrielespinosa@Gabriels-MacBook-Pro.local>
2018-05-13 01:48:32 -0700

    Fixed demon

--- version 2/Assets/EnemyFollow.cs
+++ version 2/Assets/EnemyFollow.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class EnemyFollow : MonoBehaviour {
 
 
-	public float speed;
-	public float stoppingDistance;
+	//public float speed;
+	//public float stoppingDistance;
 
-	private Transform target;
+	public Transform player;
 
 
 	// Use this for initialization
 	void Start () {
 
-		target = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+		//target = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
 	}
 	void Update()
 	{
-		if (Vector3.Distance (transform.position, target.position) > 3) {
-			transform.position = Vector3.MoveTowards (transform.position, target.position, speed * Time.deltaTime);
+		//if (Vector3.Distance (transform.position, target.position) > 3) {
+		//	transform.position = Vector3.MoveTowards (transform.position, target.position, speed * Time.deltaTime);
+		//}
+
+		if (Vector3.Distance (player.position, this.transform.position) < 100) 
+		{
+			Vector3 direction = player.position - this.transform.position;
+			direction.y = 0;
+			this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (direction), 0.1f);
+			if (direction.magnitude > 5) 
+			{
+				this.transform.Translate (0, 0, 0.05f);
+			}
 		}
 	}
 	
