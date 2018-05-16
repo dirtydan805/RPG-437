69adc25ccefb0d6b9accd1362068f4da33879191
dirtydan805 <35823973+dirtydan805@users.noreply.github.com>
2018-05-15 20:25:42 -0700

    Added flying UFO
    UFO

---
+++ version 2/Assets/Scripts/ship.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ship : MonoBehaviour {
+
+	void Update () {
+
+		transform.Rotate (new Vector3 (0,0, 90) * Time.deltaTime * 1);
+		transform.Translate (new Vector3 (0,  15, Mathf.Cos (Time.time * 16)) * Time.deltaTime);
+	}
+}
